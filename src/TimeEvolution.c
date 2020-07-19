a61925739932ecd3935be2f601873578bb109ab0
JannesWu <56634544+JannesWu@users.noreply.github.com>
2020-07-19 12:08:46 +0200

    Update TimeEvolution.c
    Add static functions for minimum and maximum search.
    Calculate the adaptive time step size.

--- src/TimeEvolution.c
+++ src/TimeEvolution.c
@@ -4,10 +4,21 @@
 #include "MainCalculations.h"
 #include "InputOutput.h"
 
+#include <stdarg.h>
 #include <math.h>
 #include <stdio.h>
 
+/* declare static functions */
 
+// Search the maximum (absolute value) velocity component in x-direction
+static double search_max_vel_u(SimulationGrid **cell, int i_max, int j_max);
+// Search the maximum (absolute value) velocity component in y-direction
+static double search_max_vel_v(SimulationGrid **cell, int i_max, int j_max);
+// Variadic function that returns the smallest double
+static double search_min_double(int n_args, ...);
+
+
+// Main time evolution function
 void time_evolution(int problem_type, double inflow_vel, double frequency, int i_max, int j_max, 
                    double reynold, double a_size, double b_size, double tau_safety, double omega_relax, 
                    double epsilon_tolerance, int max_iterations, Gravity g_accel, 
@@ -20,13 +31,19 @@ void time_evolution(int problem_type, double inflow_vel, double frequency, int i
   double int_time = 0.0;                // initialize the integration time  
   double delta_x = a_size / i_max;      // cell size in x-direction
   double delta_y = b_size / j_max;      // cell size in y-direction
+  double reynold_stability_condition = reynold / (2.0 * ((1.0 / (delta_x * delta_x)) + (1.0 / (delta_y * delta_y))));
   
   while (int_time < max_int_time) {
     // calculate adaptive size of the timestep to ensure a stable simulation: eq. (26)
-    vel_u_max_abs = ;
-    vel_v_max_abs = ;
+    vel_u_max_abs = search_max_vel_u(cell, i_max, j_max);
+    vel_v_max_abs = search_max_vel_v(cell, i_max, j_max);
 
-	delta_time = ;
+	// Catch a possible division by zero. C doesn't really care but just to be on the safe side
+	if (vel_u_max_abs == 0 || vel_v_max_abs == 0) {
+      delta_time = tau_safety * reynold_stability_condition;
+    } else {
+      delta_time = tau_safety * search_min_double(3, reynold_stability_condition, delta_x / vel_u_max_abs, delta_y / vel_v_max_abs);
+    }
 
     // implement boundary conditions: eq. (8-10)
     // the right, left and bottom boundaries are set using the no-slip condition
@@ -59,4 +76,48 @@ void time_evolution(int problem_type, double inflow_vel, double frequency, int i
 	//
 
   }
+}
+
+static double search_max_vel_u(SimulationGrid **cell, int i_max, int j_max) {
+  double max_val = 0.0;
+
+  for (int i = 1; i <= i_max; i++) {
+    for (int j = 1; j <= j_max; j++) {
+      if (max_val < fabs(cell[i][j].vel_u)) {
+        max_val = fabs(cell[i][j].vel_u);
+      }
+    }
+  }
+  return max_val;
+}
+
+static double search_max_vel_v(SimulationGrid **cell, int i_max, int j_max) {
+  double max_val = 0.0;
+
+  for (int i = 1; i <= i_max; i++) {
+    for (int j = 1; j <= j_max; j++) {
+      if (max_val < fabs(cell[i][j].vel_v)) {
+        max_val = fabs(cell[i][j].vel_v);
+      }
+    }
+  }
+  return max_val;
+}
+
+static double search_min_double(int n_args, ...) {
+  va_list num_list;
+  va_start(num_list, n_args);
+  
+  double temp_val;  
+  double min_val = va_arg(num_list, double);
+
+  for (int i = 1; i < n_args; i++) {
+    temp_val = va_arg(num_list, double);
+    if (min_val > temp_val) {
+      min_val = temp_val;
+    }
+  }
+  va_end(num_list);
+  
+  return min_val;
 }
