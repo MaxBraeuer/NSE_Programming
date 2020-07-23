6fda7e64f88ddaa94146f715147ab154540107eb
JannesWu <56634544+JannesWu@users.noreply.github.com>
2020-07-23 12:35:36 +0200

    Added missing header

---
+++ include/MemoryManager.h
@@ -0,0 +1,5 @@
+void allocate_memory(SimulationGrid ***cell, int i_max, int j_max);
+
+void initialize_cells(SimulationGrid **cell, int i_max, int j_max);
+
+void free_memory(SimulationGrid ***cell);
