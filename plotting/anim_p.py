26efcf4ac8454ab7f75480c55ee416ae00a51504
Max Bräuer <max.braeuer@uni-jena.de>
2020-07-29 17:02:01 +0200

    complete plot-scripts with animations

--- plotting/anim_p.py
+++ plotting/anim_p.py
@@ -1,8 +1,21 @@
 # FPRA- NSE
 # authors: Max Bräuer and Jannes Wulff
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.colors import BoundaryNorm
 import numpy as np
 import re
 import glob
+
+# LaTeX style output
+plt.rc('text', usetex=True)
+plt.rc('font', family='sans')
+
+# for mp4 output
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=30, metadata=dict(artist='MB'), bitrate=1800)
 
 # sort the files naturally
 def natural_sort(l):
@@ -20,3 +33,36 @@ pfilelist=filelist[0:int(len(filelist)/3)]
 fn=len(pfilelist)
 num = int(np.sqrt(len(np.loadtxt(pfilelist[0],delimiter="\n"))))
 pressure = np.zeros((num,num))
+
+# Some important plot objects
+fig = plt.figure(1, [10, 10])
+ax = fig.gca()
+# Colormap + Colorbar
+cmap = mpl.cm.get_cmap('magma')
+norm = BoundaryNorm(np.linspace(-0.1, 0.1, 10), cmap.N);
+presplot = ax.imshow(pressure, norm = norm, origin = "lower", cmap=cmap)
+bar = fig.colorbar(presplot)
+
+# get current data points
+def get_data(i):
+    return np.loadtxt(pfilelist[i],delimiter="\n").reshape((num,num))
+# initial plot configuration
+def init():
+    pressure=get_data(0)
+    presplot.set_data(pressure)
+    return presplot,
+#animation function
+def animate(i):
+    print(i)
+    pressure =get_data(i)
+    presplot.set_data(pressure)
+    return presplot,
+ani = animation.FuncAnimation(
+    fig, animate,frames=fn, init_func=init, interval=50, blit=True)
+
+plt.xlabel(r'$x$ direction')
+plt.ylabel(r"$y$ direction")
+plt.title(r"Pressure in the box")
+plt.show()
+# To save the clip
+# ani.save('im.mp4', writer=writer)
--- plotting/anim_uv.py
+++ plotting/anim_uv.py
@@ -1,8 +1,22 @@
 # FPRA- NSE
 # authors: Max Bräuer and Jannes Wulff
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.colors import BoundaryNorm
 import numpy as np
 import re
 import glob
+from skimage.transform import resize
+
+# LaTeX style output
+# plt.rc('text', usetex=True)
+# plt.rc('font', family='sans')
+
+# for mp4 output
+# Writer = animation.writers['ffmpeg']
+# writer = Writer(fps=30, metadata=dict(artist='MB'), bitrate=1800)
 
 # sort the files naturally
 def natural_sort(l):
@@ -21,3 +35,60 @@ vfilelist=filelist[2*int(len(filelist)/3)::]
 fn=len(ufilelist)
 num = int(np.sqrt(len(np.loadtxt(ufilelist[0],delimiter="\n"))))
 velo_abs = np.zeros((num,num))
+
+# Some important plot objects
+fig = plt.figure(1, [10, 10])
+ax = fig.gca()
+# Colormap + Colorbar
+cmap = mpl.cm.get_cmap('magma')
+norm = BoundaryNorm(np.linspace(-1, 1, 10), cmap.N);
+uvplot = ax.imshow(velo_abs, norm = norm, origin = "lower", cmap=cmap)
+bar = fig.colorbar(uvplot)
+X, Y = np.mgrid[0:100:10j, 0:100:10j]
+print(X)
+velo_vec = ax.quiver(Y, X, Y, X, scale=1.0, color="white")
+
+# # TEST:get current data points
+# def get_data_abs_test(i):
+#     u=np.power(X/100,2)*np.power(Y/100,2)
+#     v=u*np.sin(np.pi/10*i)
+#     return np.sqrt(np.power(u,2)+np.power(v,2))
+# def get_data_vec_test(i):
+#     u=np.power(X/100,2)*np.power(X/100,2)*np.power(Y/100,2)
+#     v=u*np.sin(np.pi/10*i)
+#     return resize(u, (10, 10)), resize(v, (10, 10))
+
+# get current data points
+def get_data_abs(i):
+    u=np.loadtxt(ufilelist[i],delimiter="\n").reshape((num,num))
+    v=np.loadtxt(vfilelist[i],delimiter="\n").reshape((num,num))
+    return np.sqrt(np.power(u,2)+np.power(v,2))
+def get_data_vec(i):
+    print(i)
+    u=np.loadtxt(ufilelist[i],delimiter="\n").reshape((num,num))
+    v=np.loadtxt(vfilelist[i],delimiter="\n").reshape((num,num))
+    return resize(u, (10, 10)), resize(v, (10, 10))
+# initial plot configuration
+def init():
+    velo_abs=get_data_abs(0)
+    uvplot.set_data(velo_abs)
+    u_res,v_res=get_data_vec(0)
+    velo_vec.set_UVC(u_res,v_res)
+    return uvplot, velo_vec
+#animation function
+def animate(i):
+    velo_abs =get_data_abs(i)
+    uvplot.set_data(velo_abs)
+    u_res,v_res=get_data_vec(i)
+    velo_vec.set_UVC(u_res,v_res)
+
+    return uvplot, velo_vec
+ani = animation.FuncAnimation(
+    fig, animate,frames=fn, init_func=init, interval=10, blit=True)
+
+plt.xlabel(r'$x$ direction')
+plt.ylabel(r"$y$ direction")
+plt.title(r"Velocities in the box")
+plt.show()
+# To save the clip
+# ani.save('im.mp4', writer=writer)
